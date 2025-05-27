0867f84443b93bc8eb738ed802d5dd24cb2f7a6d
Arthur Heberle <157285350+Arthur-Heberle@users.noreply.github.com>
2025-05-27 14:41:27 -0300

    Update Jogador.h

--- include/entidades/Jogador.h
+++ include/entidades/Jogador.h
@@ -1,25 +1,32 @@
-#ifndef JOGADOR_H
-#define ENTIDADE_QUADRADO_H
+#ifndef PLAYER_H
+#define PLAYER_H
 #include "Entidade.h"
 #include <SFML/Graphics.hpp>
+using namespace sf;
+using namespace std;
 
-class Jogador : public Entidade{
+class Player : public Sprite, Entidade {
 private:
+    const float movimentSpeed;
+    const float grid_size;
     RectangleShape shape;
+    Vector2f velocity;
+    Gerenciador_Grafico* gg;
     int player_num;
-    Gerenciador_Grafico* pGG;
+    int health;
+    const float vel_max;
+    Vector2f gravity;
+    Vector2f friction;
+    float friction_coef;
 
 public:
-    Jogador(float x, float y, float size, int p_num, Gerenciador_Grafico* pgg);
+    Player(float x, float y, Gerenciador_Grafico* pGG, int p_num);
+    ~Player();
     void handleEvent(const sf::Event& event);
-    void executar() override;
-    void desenhar() override;
-    void atualizar() override;
-    void movePlayer();
-    void colidir();
-    RectangleShape* getShape();
-    Drawable& getDrawable() override;
-    void fixCollisionWithWindow(Shape* sprite, const RenderWindow& window);
-};
+    void executar() ;
 
+    void move();
+    void colide();
+    sf::RectangleShape getShape();
+};
 #endif
