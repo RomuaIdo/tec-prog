576701aea4d4482e326b9a5c91fa4497a17ac7c4
Arthur Heberle <157285350+Arthur-Heberle@users.noreply.github.com>
2025-05-27 15:22:35 -0300

    Update and rename Ente.cpp to Ente.cpp

---
+++ src/entities/Ente.cpp
@@ -0,0 +1,12 @@
+#include "../../include/entities/Ente.h"
+#include "../../include/managers/GraphicsManager.h"
+
+GraphicsManager* Ente::pGG = GraphicsManager::getInstance();
+Ente::Ente() {
+  
+}
+
+Ente::~Ente() {
+}
+
+//Ente::pGG;
