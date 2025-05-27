4b4e64dfbb89575bd9eee7a43bcbe4320db48996
Arthur Heberle <157285350+Arthur-Heberle@users.noreply.github.com>
2025-05-27 15:25:10 -0300

    Update Entidade.h

--- include/entities/Entidade.h
+++ include/entities/Entidade.h
@@ -2,7 +2,7 @@
 #define ENTITY_H
 #include "Ente.h"
 
-class Entitye : public Ente {
+class Entity : public Ente {
 public:
   Entity();
   virtual ~Entity();
@@ -10,6 +10,6 @@ public:
   virtual void execute() = 0;
   virtual void draw() = 0;
   virtual void update() = 0;
-  virtual void colide() = 0;
+  virtual void collide() = 0;
 };
 #endif
