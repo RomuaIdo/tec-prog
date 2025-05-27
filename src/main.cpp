d9b45a843631b6d4840aa0c16a521080b74e9344
Arthur Heberle <arthurgpheberle@gmail.com>
2025-05-27 17:22:01 -0300

    fixed main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,6 @@
 #include <SFML/Window.hpp>
 #include "../include/game/Game.h"
 
-
-unsigned int const HEIGHT = 600;
-unsigned int const WIDTH  = 800;
-
-
 int main() {
     Game game;
     return 0;
