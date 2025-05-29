18e93ccf0485a05ddbcc15005e0e8689763447db
Arthur Heberle <arthurgpheberle@gmail.com>
2025-05-29 13:44:53 -0300

    fixed bugs

---
+++ src/entities/obstacles/Obstacle.cpp
@@ -0,0 +1,39 @@
+#include "../../../include/obstacles/Obstacle.h"
+#include "../../../include/managers/GraphicsManager.h"
+#include <SFML/Window.hpp>
+
+Obstacle::Obstacle(bool harm, float x, float y):
+    Entity(x,y), harmful(harm){
+    if (!texture.loadFromFile("assets/textures/Obstacle.png")) {
+        std::cerr << "Failed to load Obstacle.png!" << std::endl;
+    }    
+
+    texture.setSmooth(true);
+    sprite.setTexture(texture);
+    size.x = sprite.getLocalBounds().width;
+    size.y = sprite.getLocalBounds().height;
+    sprite.setScale(    
+    size.x / sprite.getLocalBounds().width,
+    size.y / sprite.getLocalBounds().height
+    );
+}
+
+Obstacle::~Obstacle(){
+
+}
+
+void Obstacle::execute(){
+    draw();
+}
+
+void Obstacle::ObstacleAction(){
+
+}
+
+void Obstacle::collide(){
+
+}
+
+void Obstacle::dealDamage(){
+
+}
