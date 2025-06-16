4af7588a1e5131b6d01a5a53b83002bde1c94f70
Arthur Heberle <arthurgpheberle@gmail.com>
2025-06-16 14:49:17 -0300

    commit trying to clean and fix problems

--- include/entities/characters/Enemy.h
+++ include/entities/characters/Enemy.h
@@ -7,25 +7,26 @@
 
 class Enemy : public Character {
 
-protected:
-  /* ------- PLAYER -------- */
-  list<Player *> players_list;
+    protected:
+        /* ------- PLAYER -------- */
+        list<Player*> players_list;
 
-public:
-  Enemy();
-  Enemy(float x, float y, const float acel, int life = 5, float coef = 0.5,
-        int s = 1);
-  virtual ~Enemy();
+    public:
+        Enemy();
+        Enemy(float x, float y, const float acel, int life = 5, float coef = 0.5,
+                int s = 1);
+        virtual ~Enemy();
 
-  /* ---- OWN FUNCTIONS ---- */
-  virtual void execute() = 0;
-  virtual void move() = 0;
-  virtual void collide(Entity *e) = 0;
+        /* ---- OWN FUNCTIONS ---- */
+        virtual void execute() = 0;
+        virtual void move() = 0;
+        virtual void collide(Entity *e) = 0;
 
-  /* ------- PLAYER -------- */
-  virtual void addPlayer(Player *p);
-  virtual void removePlayer(Player *p);
-  virtual void attack(Player *p) = 0;
+        /* ------- PLAYER -------- */
+        virtual void addPlayer(Player *p);
+        virtual void removePlayer(Player *p);
+        virtual void attack(Player *p) = 0;
+
 };
 
 #endif
--- src/entities/characters/Enemy.cpp
+++ src/entities/characters/Enemy.cpp
@@ -1,50 +1,17 @@
 #include "../../../include/entities/characters/Enemy.h"
-#include "../../../include/entities/characters/Player.h"
 #include <SFML/Window.hpp>
 
-
-Enemy::Enemy()
-    : Character(), players_list() {
-
-  if (!texture.loadFromFile("assets/textures/EnemySprite.png")) {
-    std::cerr << "Failed to load EnemySprite.png!" << std::endl;
-  }
-  texture.setSmooth(true);
-  sprite.setTexture(texture);
-  centerOrigin();
-  size.x = sprite.getLocalBounds().width;
-  size.y = sprite.getLocalBounds().height;
-  sprite.setScale(size.x / sprite.getLocalBounds().width,
-                  size.y / sprite.getLocalBounds().height);
-
-  players_list.clear();
+Enemy::Enemy(float x, float y, const float acel, int life, float coef , int s):
+    Character(x, y, acel, life, coef, s), players_list(){
 }
 
-Enemy::Enemy(float x, float y, const float acel, int life, float coef, int s)
-    : Character(x, y, acel, life, coef, s), players_list() {
-
-  if (!texture.loadFromFile("assets/textures/EnemySprite.png")) {
-    std::cerr << "Failed to load EnemySprite.png!" << std::endl;
-  }
-  texture.setSmooth(true);
-  sprite.setTexture(texture);
-  centerOrigin();
-  size.x = sprite.getLocalBounds().width;
-  size.y = sprite.getLocalBounds().height;
-  sprite.setScale(size.x / sprite.getLocalBounds().width,
-                  size.y / sprite.getLocalBounds().height);
-
-  players_list.clear();
+Enemy::~Enemy(){
+    players_list.clear();
 }
-
-Enemy::~Enemy() { players_list.clear(); }
-
-
 
 /* ------------------------------------------- */
 /*                OWN FUNCTIONS                */
 /* ------------------------------------------- */
-
 
 
 /* ------------------------------------------- */
@@ -52,9 +19,8 @@ Enemy::~Enemy() { players_list.clear(); }
 /* ------------------------------------------- */
 
 void Enemy::removePlayer(Player *p){
-    list<Player *>::iterator it;
-    for(it = players_list.begin(); it != players_list.end(); it++){
-        if(*it == p){
+    for(auto it = players_list.begin(); it != players_list.end(); it++){
+        if((*it) == p){
             players_list.erase(it);
             return;
         }
@@ -64,5 +30,3 @@ void Enemy::removePlayer(Player *p){
 void Enemy::addPlayer(Player *p){
     players_list.push_back(p);
 }
-
-
